c16268c678af36bfc849361901508713aca53814
xiexianbin <6504967+xiexianbin@users.noreply.github.com>
2022-06-10 16:38:42 +0800

    rm unuse return value (#1)
    Signed-off-by: xiexianbin <me@xiexianbin.cn>

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
 [![build-test](https://github.com/xiexianbin/golib/actions/workflows/workflow.yaml/badge.svg)](https://github.com/xiexianbin/golib/actions/workflows/workflow.yaml)
 [![GoDoc](https://godoc.org/github.com/xiexianbin/golib?status.svg)](https://pkg.go.dev/github.com/xiexianbin/golib)
+[![Go Report Card](https://goreportcard.com/badge/github.com/xiexianbin/golib)](https://goreportcard.com/report/github.com/xiexianbin/golib)
 
 Golang Library for xiexianbin
--- utils/map_test.go
+++ utils/map_test.go
@@ -17,16 +17,15 @@
 package utils
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestUnionMap(t *testing.T) {
 	m1 := map[string]string{"a": "a", "b": "b", "c": "c"}
 	m2 := map[string]string{"a": "a", "b": "b", "d": "d"}
-	unionMap, _ := UnionMap(m1, m2)
+	unionMap := UnionMap(m1, m2)
 	for k, v := range unionMap {
-		fmt.Println(k, v)
+		t.Log(k, v)
 	}
 }
 
@@ -34,19 +33,19 @@ func TestDiffMap(t *testing.T) {
 	type M map[string]interface{}
 	m1 := M{"a": "a", "b": "b", "c": "c", "e": "e"}
 	m2 := M{"a": "a", "b": "b", "d": "d", "e": "f"}
-	justM1, justM2, diffM1AndM2, _ := DiffMap(m1, m2)
-	fmt.Println("justM1")
+	justM1, justM2, diffM1AndM2 := DiffMap(m1, m2)
+	t.Log("justM1")
 	for k, v := range justM1 {
-		fmt.Println(k, ":", v)
+		t.Log(k, ":", v)
 	}
 
-	fmt.Println("justM2")
+	t.Log("justM2")
 	for k, v := range justM2 {
-		fmt.Println(k, ":", v)
+		t.Log(k, ":", v)
 	}
 
-	fmt.Println("diffM1AndM2")
+	t.Log("diffM1AndM2")
 	for k, v := range diffM1AndM2 {
-		fmt.Println(k, ":", v)
+		t.Log(k, ":", v)
 	}
 }
